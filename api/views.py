5f16eab0addbcd7246b925fdc9f3deefd2eb81b7
Mushfiqur719 <rahmanmushfiqur719@gmail.com>
2021-10-07 18:36:03 +0600

    1: first commit

---
+++ api/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+# Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List':'/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
